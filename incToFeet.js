631bc21f7a1a7eb9ee105df52abc3f6f8eb002cb
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-11 01:14:09 +0600

    added incToFeet file

---
+++ incToFeet.js
@@ -0,0 +1,30 @@
+function incToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+var myInches = 134;
+var feet = incToFeet(myInches);
+console.log(feet);
+
+var dadiInches = 144;
+var feet = incToFeet(dadiInches);
+console.log(feet);
+
+var naniInches = 159;
+var feet = incToFeet(naniInches);
+console.log(feet);
+
+var nanaInches = 172;
+var feet = incToFeet(nanaInches);
+console.log(feet);
+
+
+// mile to km convert
+
+function miles() {
+    km = mile / 1.60934;
+    return km;
+}
+var mile = 12;
+var km = miles(mile);
+console.log(km);
