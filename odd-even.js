807c9186f939edd587c2d60d668d05cbd3def334
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-11 03:11:03 +0600

    added code odd file

--- odd-even.js
+++ odd-even.js
@@ -1,3 +1,14 @@
-var number = 19;
-var remainder = number % 2;
-console.log(remainder == 0);
+// const number = 4;
+// const remander = number % 2;
+// console.log(remander == 0);
+
+const number = 12;
+function evenNumber(number) {
+    if (evenNumber % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+var myNumber = 23;
+var isEvenNumber = evenNumber(myNumber);
+console.log(isEvenNumber);
