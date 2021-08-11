2c1785d15cccc96c8b3f0e4bd7397275e6ac0e85
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-11 22:09:02 +0600

    added factorial file

---
+++ factorial.js
@@ -0,0 +1,12 @@
+// factorial er example..
+// 3! = 3 X 2 X 1
+// 4! = 4 X 3 X 2 X 1
+// 5! = 5 X 4 X 3 X 2 X 1
+// 7! = 7 X 6 X 5 X 4 X 3 X 2 X 1
+
+var factorial = 1;
+for (let i = 1; i <= 7; i++) {
+    console.log(i);
+    factorial = factorial * i;
+    console.log(factorial);
+}
